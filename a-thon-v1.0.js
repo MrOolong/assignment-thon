d75280e4057b25e0f665b469590ff55439296243
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-07-18 23:10:11 +0000

    function object and call

---
+++ a-thon-v1.0.js
@@ -0,0 +1,41 @@
+//function rentMe (){
+
+var sedan = {
+        type: "sedan",
+        price: 40,
+        available: "yes",
+        rentSedan: function sedan1 () {
+            //this.price = this.price - x;
+            //console.log(this.price);
+            return this.type + " " + this.price + " " + this.available;
+        }
+    };
+    
+    var suv = {
+        type: "suv",
+        price: 55,
+        available: "no",
+        rentSuv: function suv1 () {
+            return this.type + " " + this.price + " " + this.available;
+        }
+    };
+    
+    var sports = {
+        type: "sports",
+        price: 75,
+        available: "yes",
+        rentSports: function sports1 () {
+            return this.type + " " + this.price + " " + this.available;
+        }
+    };
+
+function rentalAvailability () {
+    console.log(sedan.rentSedan(1),"\n",suv.rentSuv(),"\n",sports.rentSports());
+     //doesn't work - console.log(sedan.rentSedan.sedan1());//"\n",suv.rentSuv(),"\n",sports.rentSports());
+}
+
+//}
+
+rentalAvailability();
+
+//console.log(sedan.rentSedan());
